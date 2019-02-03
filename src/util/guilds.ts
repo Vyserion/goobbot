6cfab62b0b16d90771ec7be765eae499ef2a5753
Vyserion <vyseriondev@gmail.com>
2019-02-03 18:00:39 +0000

    Update create leaderboard tests

--- src/util/guilds.ts
+++ src/util/guilds.ts
@@ -1,17 +1,11 @@
 import { UtilDao } from "./dao";
 import { Guild } from "discord.js";
-import logger from "../core/util/logger";
 
 export async function getGuildId(guild: Guild): Promise<number> {
     let botGuild = await UtilDao.getGuild(guild.id);
-    logger.info('got guild');
-    logger.info(botGuild);
     if (!botGuild) {
-        logger.info('making guild');
         botGuild = await UtilDao.createGuild(guild.id, guild.name)
-        logger.info('made guild');
     }
-    logger.info(botGuild);
 
     return botGuild.id;
 }
