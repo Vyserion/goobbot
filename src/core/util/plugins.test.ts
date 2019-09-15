b4d91881cb5ca9ef44d9226627991bb54ed22eb0
James Smith <vyseriondev@gmail.com>
2019-09-15 17:07:07 +0100

    Fix core plugin test

--- src/core/util/plugins.test.ts
+++ src/core/util/plugins.test.ts
@@ -1,8 +1,6 @@
-import "mocha";
-import { expect } from "chai";
 import { isPluginMessage } from "./plugins";
 
-describe("core/pluginManager", () => {
+describe("core/util/plugins", () => {
 	// @ts-ignore - Reads as unused variable.
 	let env;
 
@@ -14,22 +12,23 @@ describe("core/pluginManager", () => {
 	});
 
 	describe("isPluginMessage()", () => {
+
 		it("should return true when the prefix and a length is found.", () => {
 			const input: string = process.env.PREFIX + "message";
 			const result: boolean = isPluginMessage(input);
-			expect(result).to.be.true;
+			expect(result).toBeTruthy();
 		});
 
 		it("should return false when just the prefix is provided.", () => {
 			const input: string = process.env.PREFIX;
 			const result: boolean = isPluginMessage(input);
-			expect(result).to.be.false;
+			expect(result).toBeFalsy();
 		});
 
 		it("should return false when a string without the prefix is provided.", () => {
 			const input: string = "message";
 			const result: boolean = isPluginMessage(input);
-			expect(result).to.be.false;
+			expect(result).toBeFalsy();
 		});
 	});
 });
