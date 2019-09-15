38eacfd4930d375bcfb72768030d852a94ab233d
James Smith <vyseriondev@gmail.com>
2019-09-15 17:17:56 +0100

    Adds jsdoc for bot.ts

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -5,21 +5,32 @@ import { init } from "./util/dataManager";
 
 let client: Client;
 
-export async function startup() {
+/**
+ * Starts up the discord bot.
+ * External function containing all startup logic.
+ */
+export async function startup(): Promise<void> {
 	client = new Client();
 
 	await registerActions();
 	await start();
 }
 
-async function registerActions() {
+/**
+ * Registers discord actions against the created client.
+ */
+async function registerActions(): Promise<void> {
 	logger.info("Registering Discord API actions...");
 
 	await client.on("ready", onReady);
 	await client.on("message", onMessage);
 }
 
-function onReady() {
+/**
+ * Contains a set of instructions to perform when the discord connection is ready.
+ * Optionally prints the welcome message.
+ */
+function onReady(): void {
 	const showWelcomeMessage = process.env.SEND_WELCOME_MESSAGE === "true";
 
 	if (showWelcomeMessage) {
@@ -31,7 +42,11 @@ function onReady() {
 	logger.info("VyBot is ready!");
 }
 
-function onMessage(message: Message) {
+/**
+ * Contains a set of instructions to run when the bot recieves a message.
+ * @param message The message
+ */
+function onMessage(message: Message): void {
 	if (isPluginMessage(message.content)) {
 		logger.debug("Command recieved: ");
 		logger.debug("                 " + message.content);
@@ -40,12 +55,19 @@ function onMessage(message: Message) {
 	}
 }
 
+/**
+ * Starts up the internals of the bot once ready.
+ * Logs in to discord, and connects to the database pool.
+ */
 async function start() {
 	logger.info("Logging into Discord API...");
 	await client.login(process.env.APP_KEY);
 	await init();
 }
 
+/**
+ * Prints the bot welcome message to any available guilds.
+ */
 async function printWelcomeMessage() {
 	logger.info("Printing welcome messages");
 	client.guilds.forEach(guild => {
