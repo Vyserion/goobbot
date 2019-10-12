9a51c2bbfdbf0414e40a22ae0128c0209c061dd8
James Smith <vyseriondev@gmail.com>
2019-10-12 09:45:50 +0100

    Jest: Add tests for list validator

--- src/plugins/lists/dao/lists.ts
+++ src/plugins/lists/dao/lists.ts
@@ -1,45 +1,43 @@
 import { TList } from "../typings/lists";
 import { execQuery } from "../../../core/util/dataManager";
 
-export namespace Lists {
-	export async function getLists(guildId: number): Promise<TList[]> {
-		const query = `SELECT id, guild_id, name FROM lists WHERE guild_id = $1`;
-		const params = [guildId];
+export async function getLists(guildId: number): Promise<TList[]> {
+	const query = `SELECT id, guild_id, name FROM lists WHERE guild_id = $1`;
+	const params = [guildId];
 
-		const results = await execQuery<TList>(query, params);
-		return results;
+	const results = await execQuery<TList>(query, params);
+	return results;
+}
+
+export async function getList(guildId: number, name: string): Promise<TList> {
+	const query = `SELECT id, name FROM lists WHERE guild_id = $1 AND name = $2`;
+	const params = [guildId, name];
+
+	const results = await execQuery<TList>(query, params);
+	if (results.length > 0) {
+		return results[0];
+	} else {
+		return null;
 	}
+}
 
-	export async function getList(guildId: number, name: string): Promise<TList> {
-		const query = `SELECT id, name FROM lists WHERE guild_id = $1 AND name = $2`;
-		const params = [guildId, name];
+export async function createList(guildId: number, name: string): Promise<void> {
+	const query = `INSERT INTO lists VALUES (DEFAULT, $1, $2)`;
+	const params = [guildId, name];
 
-		const results = await execQuery<TList>(query, params);
-		if (results.length > 0) {
-			return results[0];
-		} else {
-			return null;
-		}
-	}
+	await execQuery(query, params);
+}
 
-	export async function createList(guildId: number, name: string): Promise<void> {
-		const query = `INSERT INTO lists VALUES (DEFAULT, $1, $2)`;
-		const params = [guildId, name];
+export async function updateListName(guildId: number, name: string, newName: string): Promise<void> {
+	const query = `UPDATE lists SET name = ($1) WHERE name LIKE ($2) AND guild_id = $3`;
+	const params = [newName, name, guildId];
 
-		await execQuery(query, params);
-	}
+	await execQuery(query, params);
+}
 
-	export async function updateListName(guildId: number, name: string, newName: string): Promise<void> {
-		const query = `UPDATE lists SET name = ($1) WHERE name LIKE ($2) AND guild_id = $3`;
-		const params = [newName, name, guildId];
+export async function deleteList(guildId: number, name: string): Promise<void> {
+	const query = `DELETE FROM lists WHERE guild_id = $1 AND name LIKE ($2)`;
+	const params = [guildId, name];
 
-		await execQuery(query, params);
-	}
-
-	export async function deleteList(guildId: number, name: string): Promise<void> {
-		const query = `DELETE FROM lists WHERE guild_id = $1 AND name LIKE ($2)`;
-		const params = [guildId, name];
-
-		await execQuery(query, params);
-	}
+	await execQuery(query, params);
 }
--- src/plugins/lists/utils/validators.ts
+++ src/plugins/lists/utils/validators.ts
@@ -1,6 +1,13 @@
-import { Lists } from "../dao/lists";
+import { getList } from "../dao/lists";
 
+/**
+ * Checks if a list exists.
+ * @param listName The list name to check
+ * @param guildId The guild id the list should belong to
+ * 
+ * @returns True if the list exists, false otherwise
+ */
 export async function listExists(listName: string, guildId: number): Promise<boolean> {
-	const listExists = await Lists.getList(guildId, listName);
+	const listExists = await getList(guildId, listName);
 	return !!listExists;
 }
